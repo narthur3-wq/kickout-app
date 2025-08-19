44da0734540516911e2a33d053a8fa39f3f65ff5
narthur3-wq <neil.arthur3@gmail.com>
2025-08-19 15:36:50 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,2 +1,9 @@
-import App from './App.svelte';
-new App({ target: document.getElementById('app') });
+import { mount } from 'svelte'
+import './app.css'
+import App from './App.svelte'
+
+const app = mount(App, {
+  target: document.getElementById('app'),
+})
+
+export default app
