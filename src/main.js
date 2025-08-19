30008e214518a802ed4d84b69e97f53d26decccf
narthur3-wq <neil.arthur3@gmail.com>
2025-08-19 13:57:29 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,3 +1,3 @@
-import App from './App.svelte'
-const app = new App({ target: document.getElementById('app') })
-export default app
+import App from './App.svelte';
+const app = new App({ target: document.getElementById('app') });
+export default app;
