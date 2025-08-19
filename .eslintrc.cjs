60bce4de30523f20ba09f3667c66d8df406ba374
narthur3-wq <neil.arthur3@gmail.com>
2025-08-19 20:26:30 +0100

    Update .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,27 +1,12 @@
 module.exports = {
   root: true,
-  env: {
-    browser: true,
-    es2021: true,
-    node: true,
-  },
-  extends: [
-    "eslint:recommended",
-    "plugin:svelte/recommended",
-  ],
-  overrides: [
-    {
-      files: ["*.svelte"],
-      processor: "svelte/svelte",
-    },
-  ],
-  parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-  },
+  env: { browser: true, es2021: true, node: true },
+  extends: ["eslint:recommended", "plugin:svelte/recommended"],
+  overrides: [{ files: ["*.svelte"], processor: "svelte/svelte" }],
+  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
   plugins: ["svelte"],
   rules: {
-    // Example: allow console.log during dev, warn in prod
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-  },
+    "no-console": "warn",
+    "no-unused-vars": ["warn", { argsIgnorePattern: "^_" }]
+  }
 };
