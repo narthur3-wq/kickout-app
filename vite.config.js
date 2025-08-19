d4086237422f52a60bc95228ab3fd9e4f82d3c29
narthur3-wq <neil.arthur3@gmail.com>
2025-08-19 12:51:21 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 export default defineConfig({
   plugins: [svelte()],
-  // IMPORTANT for GitHub Pages under /kickout-app/
-  base: '/kickout-app/',
+  base: '/kickout-app/', // important for GitHub Pages
 })
