bd1f91f79779a9aac79c23fbfe805b31d0753ccc
narthur3-wq <neil.arthur3@gmail.com>
2025-08-27 16:59:50 +0100

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,23 +1,5 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
-
 module.exports = {
   content: ["./index.html","./src/**/*.{svelte,js,ts}"],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Inter', ...defaultTheme.fontFamily.sans],
-      },
-      colors: {
-        win: '#16A34A',
-        loss: '#EF4444',
-        neutral: '#D1D5DB',
-        background: '#F9FAFB',
-        accent: '#2563EB',
-      },
-      borderRadius: {
-        'xl': '0.875rem',
-      }
-    },
-  },
+  theme: { extend: {} },
   plugins: [],
 };
